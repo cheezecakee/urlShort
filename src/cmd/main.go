bcd742a9651b498e2831ed05a03704983fa65096
Anabel <anabel.pmoraes@gmail.com>
2024-06-05 12:02:50 -0300

    organized files.

---
+++ src/cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	_ "fmt"
+)
+
+func main() {
+	// Create map for long and short url
+	// short url redirects to long url
+	// first check yaml path
+	// check map path
+	// check default
+}
